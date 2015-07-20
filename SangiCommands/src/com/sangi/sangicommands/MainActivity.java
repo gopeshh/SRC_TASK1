bbde1865b1f48fab9323249ce094fce573f36f56
Srinivas <sairao1996@gmail.com>
2015-07-21 01:15:47 +0530

    Refershed the branch

---
+++ SangiCommands/src/com/sangi/sangicommands/MainActivity.java
@@ -0,0 +1,216 @@
+package com.sangi.sangicommands;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.speech.RecognizerIntent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	private Button recordButton;
+	private Button bluetoothButton;
+	private Button wifiButton;
+	private Button stemmerTest;
+	private EditText translatedText;
+	
+	//Just a code required for onActivityResult
+	private static final int REQUEST_CODE = 1234;
+	//Starts the bluetooth and makes device available for the DISCOVER_DURATION
+	private static final int DISCOVER_DURATION = 300;
+	
+	private static final int REQUEST_BLU = 1;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //Assign the views to objects
+        recordButton = (Button) findViewById(R.id.speechStart);
+        translatedText = (EditText) findViewById(R.id.translatedText);
+        bluetoothButton = (Button) findViewById(R.id.sendDataBluetooth);
+        wifiButton = (Button) findViewById(R.id.sendDataIce);
+        stemmerTest = (Button) findViewById(R.id.stemmerTest);
+        
+        //Set listeners
+        recordButton.setOnClickListener(this);
+        bluetoothButton.setOnClickListener(this);
+        wifiButton.setOnClickListener(this);
+        stemmerTest.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+	@Override
+	public void onClick(View v) {
+		if ( recordButton.isPressed() )
+		{
+			startVoiceRecognitionActivity();
+		}
+		if(bluetoothButton.isPressed())
+		{
+			sendViaBluetooth();
+		}
+		if(wifiButton.isPressed())
+		{
+			sendViaWifi();
+		}
+		if(stemmerTest.isPressed())
+		{
+			performStemming();
+		}
+	}
+	
+	public void performStemming(){
+		englishStemmer stemmer= new englishStemmer();
+		//Sets the text for stemming
+		String textToStem = translatedText.getText().toString();
+		String[] split = textToStem.split("\\s+"); 
+		for(int i = 0; i<split.length;i++){
+			//Set the text to stem
+			stemmer.setCurrent(split[i]);
+			//Stems the text
+			stemmer.stem();
+			split[i]=stemmer.getCurrent();
+		}
+		String stemmedText=split[0];
+		
+		for(int i = 1; i<split.length; i++){
+			stemmedText = stemmedText +" "+split[i];
+		}
+		//Return Stemmed Text
+		translatedText.setText(stemmedText);
+	}
+	
+	public void sendViaWifi(){
+		//Send data over the wifi
+		
+		
+		
+	}
+	
+	 public void startVoiceRecognitionActivity() {
+				    
+		 	Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+			intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+			                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+			intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice recognition Demo...");
+			startActivityForResult(intent, REQUEST_CODE);
+			        
+	 }
+	    
+	 protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		 if(resultCode == DISCOVER_DURATION && requestCode == REQUEST_BLU) {
+	    		
+	    		Intent intent = new Intent();
+	    		intent.setAction(Intent.ACTION_SEND);
+	    		intent.setType("text/plain");
+	    		File sdcard = Environment.getExternalStorageDirectory();
+	    		//Get the text file
+	    		File file = new File(sdcard,"Commands.txt");
+	    		FileOutputStream fos;
+	            try {
+	            	fos = new FileOutputStream(file);
+					fos.write(translatedText.getText().toString().getBytes());
+					fos.flush();
+		            fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	            
+	    		intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+	    		
+	    		PackageManager pm = getPackageManager();
+	    		List<ResolveInfo> appsList = pm.queryIntentActivities(intent, 0);
+	    		
+	    		if(appsList.size() > 0) {
+	    			String packageName = null;
+	    			String className = null;
+	    			boolean found = false;
+	    			
+	    			for(ResolveInfo info : appsList) {
+	    				packageName = info.activityInfo.packageName;
+	    				if(packageName.equals("com.android.bluetooth")) {
+	    					className = info.activityInfo.name;
+	    					found = true;
+	    					break;
+	    				}
+	    			}
+	    			
+					if (!found) {
+						Toast.makeText(this, "Bluetooth havn't been found",
+								Toast.LENGTH_LONG).show();
+					} else {
+						intent.setClassName(packageName, className);
+						startActivity(intent);
+					}
+	    		}
+			} 
+		 
+		 if (requestCode == REQUEST_CODE && resultCode== -1) {
+		      ArrayList matches = data
+		          .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+		      translatedText.setText((String) matches.get(0));
+		    }
+		    super.onActivityResult(requestCode, resultCode, data);
+		    
+		    
+	 }
+	 
+	 public void sendViaBluetooth(){
+		 	BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+	    	
+	    	if(btAdapter == null) {
+	    		Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_LONG).show();
+	    	} else {
+	    		enableBluetooth();
+	    	}
+	 }
+	 
+	 public void enableBluetooth() {
+	    	
+	    	Intent discoveryIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+	    	
+	    	discoveryIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVER_DURATION);
+	    	
+	    	startActivityForResult(discoveryIntent, REQUEST_BLU);
+	    }
+}
+
